8549acd51411f7ffd8d275e68fbec92617e433ab
mxcoogi <mxcoogi@gmail.com>
2025-03-25 12:52:46 +0900

    refactor : Add AuthorController

---
+++ src/main/java/org/example/taskmanager/task/controller/AuthorController.java
@@ -0,0 +1,12 @@
+package org.example.taskmanager.task.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/author")
+public class AuthorController {
+
+
+}
