9ecacc397369d84ccc1e3f85242e0fcba9f989ac
mxcoogi <mxcoogi@gmail.com>
2025-03-26 18:54:35 +0900

    feat : Add valid max-length(200) taskName

--- src/main/java/org/example/taskmanager/task/dto/TaskRequestDto.java
+++ src/main/java/org/example/taskmanager/task/dto/TaskRequestDto.java
@@ -2,17 +2,16 @@ package org.example.taskmanager.task.dto;
 
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.Setter;
-
-import java.time.LocalDate;
 
 
 @Getter
 public class TaskRequestDto {
 
     @NotBlank
+    @Max(200)
     private String taskName;
     @Email
     @NotBlank
